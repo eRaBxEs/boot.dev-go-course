256b49279d21ca4098cd5ee4963eb13e9a9e188f
Ehioje Henry Erabor <ehiojee@yahoo.com>
2025-08-29 18:04:51 +0100

    use of type assertion type switch expression

---
+++ lessonEinterface5/go.mod
@@ -0,0 +1,3 @@
+module interface-5
+
+go 1.25.0
---
+++ lessonEinterface5/main.go
@@ -0,0 +1,107 @@
+package main
+
+import "fmt"
+
+/*
+Assignment
+After submitting our last snippet of code for review, a more experienced gopher told us to use a type switch instead of successive assertions. Let's make that improvement!
+
+Implement the getExpenseReport function using a type switch.
+
+	If the expense is an email, return the email's toAddress and the cost of the email.
+	If the expense is an sms, return the sms's toPhoneNumber and its cost.
+	If the expense has any other underlying type, return an empty string and 0.0 for the cost.
+*/
+
+func getExpenseReport(e expense) (string, float64) {
+
+	switch v := e.(type) {
+	case email:
+		return v.toAddress, v.cost()
+	case sms:
+		return v.toPhoneNumber, v.cost()
+	default:
+		return "", 0.0
+
+	}
+
+}
+
+// don't touch below this line
+
+type expense interface {
+	cost() float64
+}
+
+type email struct {
+	isSubscribed bool
+	body         string
+	toAddress    string
+}
+
+type sms struct {
+	isSubscribed  bool
+	body          string
+	toPhoneNumber string
+}
+
+type invalid struct{}
+
+func (e email) cost() float64 {
+	if !e.isSubscribed {
+		return float64(len(e.body)) * .05
+	}
+	return float64(len(e.body)) * .01
+}
+
+func (s sms) cost() float64 {
+	if !s.isSubscribed {
+		return float64(len(s.body)) * .1
+	}
+	return float64(len(s.body)) * .03
+}
+
+func (i invalid) cost() float64 {
+	return 0.0
+}
+
+func test(e expense) {
+	address, cost := getExpenseReport(e)
+	switch e.(type) {
+	case email:
+		fmt.Printf("Report: The email is going to %s; will cost %.2f\n", address, cost)
+		fmt.Println("========================================================================")
+	case sms:
+		fmt.Printf("Report: The sms is going to %s; will cost %.2f\n", address, cost)
+		fmt.Println("========================================================================")
+	default:
+		fmt.Println("Report: Invalid Expense")
+		fmt.Println("========================================================================")
+	}
+}
+
+func main() {
+	test(email{
+		isSubscribed: true,
+		body:         "Hello there",
+		toAddress:    "johndoe@gmail.com",
+	})
+
+	test(email{
+		isSubscribed: false,
+		body:         "This meeting could have been an email",
+		toAddress:    "john@doe.com",
+	})
+
+	test(sms{
+		isSubscribed:  false,
+		body:          "We are expecting you home",
+		toPhoneNumber: "+13666377",
+	})
+
+	test(sms{
+		isSubscribed:  true,
+		body:          "welcome to the factory",
+		toPhoneNumber: "+444667748",
+	})
+}

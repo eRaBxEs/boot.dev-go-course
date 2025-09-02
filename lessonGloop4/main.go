0f0061b61aaa0bb6d3231c86295ea319904fb922
Ehioje Henry Erabor <ehiojee@yahoo.com>
2025-09-02 09:00:14 +0100

    solve the legendary fizzbuzz task

---
+++ lessonGloop4/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+/*
+We're hiring engineers at Textio, so time to brush up on the classic "Fizzbuzz" game, a coding exercise that has been dramatically overused in coding interviews across the world.
+
+Complete the fizzbuzz function that prints the numbers 1 to 100 inclusive each on their own line, but replace multiples of 3 with the text fizz and multiples of 5 with buzz. Print fizzbuzz for multiples of 3 AND 5.
+*/
+
+func fizzbuzz() {
+	for i := 1; i <= 100; i++ {
+		if i%3 == 0 && i%5 == 0 {
+			fmt.Println("fizzbuzz")
+		} else if i%3 == 0 {
+			fmt.Println("fizz")
+		} else if i%5 == 0 {
+			fmt.Println("buzz")
+		} else {
+			fmt.Println(i)
+		}
+	}
+}
+
+// don't touch below this line
+
+func main() {
+	fizzbuzz()
+}

a7717cd6563c006c2a0d682650e59ac366fffd9e
Ehioje Henry Erabor <ehiojee@yahoo.com>
2025-09-06 10:22:36 +0100

    add usage to test function

--- lessonHSlice5/main.go
+++ lessonHSlice5/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 /*
 We need to be able to quickly detect bad words in the messages our system sends.
 
@@ -18,4 +20,20 @@ func indexOfFirstBadWord(msg []string, badWords []string) int {
 
 	}
 	return -1
+}
+
+func test(msg []string, badWords []string) {
+	fmt.Println("=========================================================")
+	fmt.Printf("The list of words that we intend to check for bad words includes: %#v\n", msg)
+	fmt.Printf("The list of bad words we are checking for includes: %#v\n", badWords)
+	index := indexOfFirstBadWord(msg, badWords)
+	fmt.Printf("The index of the first bad word is: %v\n", index)
+
+}
+
+func main() {
+	test([]string{"Jesus", "Great", "f**ck", "God", "wonder"}, []string{"f**ck", "shit", "poop", "pile"})
+	test([]string{"Peace", "Joy", "Amazing", "Gentleness", "sh**t"}, []string{"f**ck", "shit", "poop", "pile"})
+	test([]string{"Peace", "Joy", "Complexity", "Gentleness", "shit"}, []string{"f**ck", "shit", "poop", "pile"})
+	test([]string{"Happy", "Cry", "Deck", "Glory", "Fullness", "sex", "male"}, []string{"f**ck", "shit", "sex", "poop", "pile"})
 }

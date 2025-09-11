2350511f64db0e89cd64be8395c5782cf0a063ee
Ehioje Henry Erabor <ehiojee@yahoo.com>
2025-09-11 05:05:44 +0100

    use of pointer in adjusting data

---
+++ lessonKPointers2/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+/*
+Assignment
+Complete the removeProfanity function.
+
+It should use the strings.ReplaceAll function to replace all instances of the following words in the input message with asterisks.
+
+Word	Replacement
+fubb	****
+shiz	****
+witch	*****
+It should update the value in the pointer and return nothing.
+
+Do not alter the function signature.
+*/
+
+func removeProfanity(message *string) {
+	messageVal := *message
+	messageVal = strings.ReplaceAll(messageVal, "fubb", "****")
+	messageVal = strings.ReplaceAll(messageVal, "shiz", "****")
+	messageVal = strings.ReplaceAll(messageVal, "witch", "*****")
+	*message = messageVal // passing the value back into the pointer
+}
+
+func test(messages []string) {
+	for _, message := range messages {
+		removeProfanity(&message)
+		fmt.Println(message)
+	}
+}
+
+func main() {
+	test([]string{"You are not a fubb", "This is all shiz", "She is an old witch"})
+}
